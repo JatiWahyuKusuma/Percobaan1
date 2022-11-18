d652573b2bc4da05b87064739fd29be6ffebaddf
JatiWahyuKusuma <jatiikusuma@gmail.com>
2022-11-18 09:45:22 +0700

    first commit

---
+++ src/Ikan.java
@@ -0,0 +1,7 @@
+public  class Ikan extends Hewan {
+    
+    @Override
+    public void bergerak(){
+        System.out.println("Berjalan Derngan KAKI, \"Tap...tap...\"");
+    }
+}
---
+++ src/Kucing.java
@@ -0,0 +1,8 @@
+
+public  class Kucing extends Hewan{
+    
+    @Override
+    public void bergerak(){
+        System.out.println("Berenang Dengan SIRIP, \"wush......\"");
+    }
+}
